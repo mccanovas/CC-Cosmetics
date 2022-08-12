53bd359033df9487f1c7d369597d82ae09ca7255
Camila Canovas <mccanovas@outlook.com>
2022-08-12 04:22:50 -0300

    adding DOM and JSON

--- scripts/products.js
+++ scripts/products.js
@@ -1,35 +1,92 @@
-//Script para el contador
-let btnIncrementar = document.getElementById('incrementar');
-let btnDecrementar = document.getElementById('decrementar');
-let contador = document.getElementById('contador');
-let contadorValue = parseInt(contador.innerText);
+//Declaro mis productos en un array de objetos
+const productos = [
+    {id:1, tipo:"Labial", nombre:"Labial Matte Red", precio: 1200, foto:"./images/Lrojo.jpg" },
+    {id:2, tipo:"Labial", nombre:"Labial Matte Pink", precio: 1200,foto:"./images/Lrosa.jpg" },
+    {id:3, tipo:"Labial", nombre:"Labial Matte Nude", precio: 1200, foto:"./images/Lnude.jpg" },
+    {id:4, tipo:"Crema", nombre:"Crema de día", precio: 1800, foto:"./images/cdia.jpg" },
+    {id:5, tipo:"Crema", nombre:"Crema de noche", precio: 1800, foto:"./images/cnoche.jpg" },
+    {id:6, tipo:"Crema", nombre:"Crema exfoliante", precio: 2100, foto:"./images/cexf.jpg" },
+  ]
+  
+  //inicializo la variable carrito con una funcion para que detecte si existen valores en el storage
+  let carrito = cargarCarrito();
+  window.addEventListener('load',calcularTotalCarrito);
+  
+  //Inicializo las variables del HTML
+  let sectionProductos = document.getElementById("section-productos");
+  let sectionCarrito = document.getElementById("section-carrito");
+  
+  //Creo los productos con DOM
+  let totalCompra = document.createElement("div");
+  totalCompra.innerHTML = "<h3>Total: $</h3>";
+  sectionCarrito.appendChild(totalCompra);
+  
+  let montoTotalCompra = document.createElement("h3");
+  montoTotalCompra.innerText = "0";
+  totalCompra.appendChild(montoTotalCompra);
+  
+  let cantidadProductos = document.createElement("div");
+  cantidadProductos.innerHTML = "<h3>Cantidad de productos: </h3>";
+  sectionCarrito.appendChild(cantidadProductos);
+  
+  let cantProductos = document.createElement("h3");
+  cantProductos.innerText = "0";
+  cantidadProductos.appendChild(cantProductos);
+  
+  let botonFinalizar = document.createElement("button");
+  botonFinalizar.innerText = "Finalizar compra";
+  sectionCarrito.appendChild(botonFinalizar);
+  botonFinalizar.setAttribute("class", "boton");
+  
+  //Vacío el carrito al finalizar la compra
+  botonFinalizar.onclick = () => {vaciarCarrito()};
+  
+  // Productos que se muestran en cards
+  for (const producto of productos) {
+    let container = document.createElement("div");
+    container.setAttribute("class", "card-product");
+    container.innerHTML = ` <div class="img-container">
+                              <img src="${producto.foto}" alt="${producto.nombre}" class="img-product"/>
+                              </div>
+                              <div class="info-producto">
+                              <p class="font">${producto.nombre}</p>
+                              <strong class="font">$${producto.precio}</strong>
+                              <button class="boton" id="btn${producto.id}"> Agregar al carrito </button>
+                              </div>`;
+    sectionProductos.appendChild(container);
 
-function handleContador(e) {
-    let btn = e.target;
-    let valor = btn.innerText;
-    if (valor == '+' && contadorValue <50) {
-        contadorValue++;
-        contador.innerText = contadorValue;
-    } else if (valor == '-' && contadorValue >0) {
-        contadorValue--;
-        contador.innerText = contadorValue;
+    //Evento para que los productos se agreguen al carrito al hacer click en el boton
+    document.getElementById(`btn${producto.id}`).onclick = () => agregarAlCarrito(`${producto.id}`);
+  }
+  
+  //Funciones
+  function agregarAlCarrito(id) {
+    carrito.push(productos.find(p => p.id == id));
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    calcularTotalCarrito();
+  }
+  
+  function calcularTotalCarrito() {
+    let total = 0;
+    for (const producto of carrito) {
+      total += producto.precio;
     }
-}
-
-btnIncrementar.addEventListener('click', handleContador);
-btnDecrementar.addEventListener('click', handleContador);
-
-//Declaro el valor del producto
-const precioLabial = 1200;
-
-//Script para indicar qué se agrega al carrito
-
-let sumarCarrito = document.getElementById ('btnCarrito');
-//Función para el mensaje
-function handleBtnCarrito () {
-    let mensaje = document.createElement("p");
-    mensaje.innerHTML = "Agregaste " + contadorValue + " unidades del labial Matte Red al carrito y abonarás $" + (contadorValue*precioLabial)+" en total."; 
-    document.body.append(mensaje);
-}
-//Llamado a la función
-sumarCarrito.addEventListener ('click', handleBtnCarrito);
+    montoTotalCompra.innerText = total;
+    cantProductos.innerText = carrito.length;
+  }
+  
+  function vaciarCarrito() {
+    montoTotalCompra.innerText = "0";
+    cantProductos.innerText = "0";
+    localStorage.clear();
+    carrito = [];
+  }
+  
+  function cargarCarrito() {
+    let carrito = JSON.parse(localStorage.getItem("carrito"));
+    if (carrito == null) {
+      return [];
+    } else {
+      return carrito;
+    }
+  }
